49bc0be56c2d35da52f64cc1c85630d11a29cfa2
Nico Beta <nico1485@gmail.com>
2016-09-24 18:00:56 -0300

    Add new folders

---
+++ src/Laito/Core/Base.php
@@ -0,0 +1,22 @@
+<?php namespace Laito\Core;
+
+use Laito\App;
+
+class Base
+{
+
+    /**
+     * @var App $app Application instance
+     */
+    public $app;
+
+    /**
+     * Constructor
+     *
+     * @param App $app Application instance
+     */
+    public function __construct(App $app) {
+        $this->app = $app;
+    }
+
+}
